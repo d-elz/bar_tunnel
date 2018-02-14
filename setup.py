9e93338d46995b9bd3bef4b363d8dca6634bea0b
delz <kwspoul@gmail.com>
2018-02-14 22:55:35 +0200

    making a setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+#/bin/bash
+import os
+import sqlite3 as lite
+
+os.makedirs('/keys')
+con = lite.connect('bar/db/bar.db')
+
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE contacts(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, label TEXT, publickey TEXT, sharedkey TEXT)")
+    cur.execute("CREATE TABLE clients(id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, port INT, timelogout TEXT)")
+    cur.execute("CREATE TABLE messages(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)")
+    cur.execute("CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT, time TEXT)")
+
+cur.execute("CREATE UNIQUE INDEX 'id_UNIQUE' ON 'contacts' ('id' ASC);")
