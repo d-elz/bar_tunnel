f481938d68820cd57296e69abd81e0ab0fa3677b
delz <kwspoul@gmail.com>
2018-02-14 23:01:35 +0200

    making a setup.py file

--- setup.py
+++ setup.py
@@ -7,9 +7,4 @@ con = lite.connect('bar_tunnel/db/bar.db')
 
 with con:
     cur = con.cursor()
-    cur.execute("CREATE TABLE contacts(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, label TEXT, publickey TEXT, sharedkey TEXT)")
-    cur.execute("CREATE TABLE clients(id INTEGER PRIMARY KEY AUTOINCREMENT, ip TEXT, port INT, timelogout TEXT)")
-    cur.execute("CREATE TABLE messages(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT)")
-    cur.execute("CREATE TABLE history(id INTEGER PRIMARY KEY AUTOINCREMENT, message TEXT, time TEXT)")
-
-cur.execute("CREATE UNIQUE INDEX 'id_UNIQUE' ON 'contacts' ('id' ASC);")
+    cur.execute("CREATE TABLE List(id INTEGER PRIMARY KEY AUTOINCREMENT, nym TEXT, pk TEXT, shared_key TEXT, label TEXT)")
